ac76a4a8e6668d24091554b3e4cc229b0e4e9eec
Matheus Fuchs Signorini <matheusfuchssig@hotmail.com.br>
2021-06-04 09:39:27 -0300

    Update

--- CursoJS/site_camisas/js/scripts.js
+++ CursoJS/site_camisas/js/scripts.js
@@ -65,18 +65,6 @@ var parametros_pesquisa = {
 }
 
 
-// Regras adicionais para o orçamento:
-
-// 1. Verificar se há em localStorage os parâmetros do último orçamento e se houver, carregar a página com eles.
-
-// 2. A camisa de qualidade alta (190g/m2) deve acrescer o preço unitário em 12%.
-
-// 3. A embalagem unitária tem um custo de 0.15 por unidade
-
-// 4. Após cálculo do preço, há que se aplicar um desconto por quantidade, sendo: 
-    // faixa 1: acima de 1.000 - Desconto de 15%
-    // faixa 2: acima de 500 - Desconto de 10%
-    // faixa 3: acima de 100 - Desconto de 5%
 
 
 
@@ -272,30 +260,4 @@ $(function(){
 
 
 
-// Sugestão de etapas da resolução
-
-    // 1. Crie uma função para calcular o preço baseado nos parâmetros da variável "parametros_pesquisa" e solte o 
-    // valor no console para testar se está certo.
-
-    // 2. Faça os eventos click e change para os filtros.
-    
-        // a. Faça o evento click para os filtros do tipo botão (.option-filter). Sempre que houver um click, 
-        // remova a classe "selected" dos botões do grupo e depois aplique-a apenas ao que foi clicado para
-        // que ele fique azul.
-
-        // b. Faça o evento change para os filtros do tipo <select> e para o <input> de quantidade.
-
-        // c. Sempre que um dos eventos acima ocorrer, atualize a variável "parametros_pesquisa" e rode a função para 
-        // calcular o preço
-
-    
-    // 3. Altere a função do cálculo do preço. Em vez de soltar os valores no console, atualize as informações
-    // nos elementos "result_", atualize o preço no elemento "valor-total" e mude o atributo "src" do elemento 
-    // "foto-produto" para alterar a imagem mostrada (todas as imagens estão na pasta img).
-
-    // 4. Adicione a funcionalidade de hide e show do spinner (elemento "refresh-loader") à função de cálculo de preço. 
-    // Como não estamos consultando dados externos, o cálculo acaba sendo rápido demais, portanto use um setTimeout 
-    // para deixar ele aparecer por pelo menos 2 segundos.
-
-    // 5. Crie a funcionalidade do localStorage e ao carregar a página, consulte o localStorage, 
-    // atualize a variável "parametros_pesquisa" e rode a função de cálculo de preço
+/
